bc59324dd0012c4bcc193a43961ea31f71f98d3d
roman <romanrus98@mail.ru>
2025-04-14 23:45:06 +0500

    add check error ai

--- src/migrations/20250406122530-create-lobby-answer.ts
+++ src/migrations/20250406122530-create-lobby-answer.ts
@@ -24,7 +24,7 @@ module.exports = {
         type: Sequelize.INTEGER,
         allowNull: false,
         references: {
-          model: 'Users',
+          model: 'users',
           key: 'id',
         },
         onDelete: 'CASCADE',
