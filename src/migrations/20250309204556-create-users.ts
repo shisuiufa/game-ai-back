76306f2507aaa1251ce2907daa59676cd5f0ec8a
roman <romanrus98@mail.ru>
2025-04-16 02:16:29 +0500

    add reconnect

--- src/migrations/20250309204556-create-users.ts
+++ src/migrations/20250309204556-create-users.ts
@@ -27,6 +27,11 @@ module.exports = {
         allowNull: false,
         defaultValue: "user",
       },
+      points: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        defaultValue: 0,
+      },
       createdAt: {
         type: DataTypes.DATE,
         allowNull: false,
